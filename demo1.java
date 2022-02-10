cc989192ec697aecc78825065b88dcc3ce14347d
First Last <sksatyab4ugmail@ip-172-31-95-89.ec2.internal>
2022-02-10 07:09:56 +0000

    demo1 1st commit

---
+++ demo1.java
@@ -0,0 +1,13 @@
+Hi, 
+Full X server and SSH support
+Remote desktop (RDP, VNC, Xdmcp)
+Remote terminal (SSH, telnet, rlogin, Mosh)
+X11-Forwarding
+Automatic SFTP browser
+Master password protection
+Plugins support
+Portable and installer versions
+Full documentation
+Max. 12 sessions
+Max. 2 SSH tunnels
+Max. 4 macros
